a57baca050a826fbf222daacc34d579a28938860
Robert Schittny <robert.schittny@sintef.no>
2018-12-18 14:18:11 +0100

    Add more query functions

--- clfpy/cli/cli_tools.py
+++ clfpy/cli/cli_tools.py
@@ -1,4 +1,40 @@
 import sys
+import os
+
+
+def query_int(question, default=None):
+    if default is None:
+        prompt = " "
+    elif isinstance(default, int):
+        prompt = f" [{default}] "
+    else:
+        raise ValueError(f"Invalid default answer {default}")
+
+    while True:
+        sys.stdout.write(question + prompt)
+        choice = input()
+        if default is not None and choice == '':
+            return default
+        else:
+            try:
+                value = int(choice)
+            except ValueError:
+                sys.stdout.write("Please enter an integer\n")
+                continue
+            return value
+
+
+def query_filepath(question):
+    prompt = " "
+    while True:
+        sys.stdout.write(question + prompt)
+        choice = input()
+        if not os.path.isfile(choice):
+            print("Please enter a valid path to a file")
+            continue
+        else:
+            return choice
+
 
 def query_yes_no(question, default="yes"):
     """Ask a yes/no question via input() and return the answer.
