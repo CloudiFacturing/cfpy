136f69c0f8df60e4ea9495389c95069ef3664bd7
Robert Schittny <robert.schittny@sintef.no>
2018-12-18 09:14:36 +0100

    Move query_yes_no to separate module

--- clfpy/cli/cli_gss.py
+++ clfpy/cli/cli_gss.py
@@ -7,45 +7,14 @@ sys.path.append("../..")
 
 import clfpy as cf
 
+from cli_tools import query_yes_no
+
 GSS_endpoint = "https://api.hetcomp.org/gss-0.1/FileUtilities?wsdl"
 
 GSS_roots = [
         "it4i_anselm://",
         "it4i_salomon://"
 ]
-
-
-def query_yes_no(question, default="yes"):
-    """Ask a yes/no question via input() and return the answer.
-
-    "question" is a string that is presented to the user.
-    "default" is the presumed answer if the user just hits <Enter>.
-        It must be "yes" (the default), "no" or None (meaning
-        an answer is required of the user).
-
-    The "answer" return value is True for "yes" or False for "no".
-    """
-    valid = {"yes": True, "y": True,
-             "no": False, "n": False}
-    if default is None:
-        prompt = " [y/n] "
-    elif default == "yes":
-        prompt = " [Y/n] "
-    elif default == "no":
-        prompt = " [y/N] "
-    else:
-        raise ValueError("Invalid default answer: '%s'" % default)
-
-    while True:
-        sys.stdout.write(question + prompt)
-        choice = input().lower()
-        if default is not None and choice == '':
-            return valid[default]
-        elif choice in valid:
-            return valid[choice]
-        else:
-            sys.stdout.write("Please respond with 'yes' or 'no' "
-                             "(or 'y' or 'n').\n")
 
 
 class GssCLI(cmd.Cmd):
